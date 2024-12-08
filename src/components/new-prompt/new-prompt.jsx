7daf07942871a1888ab97e57a7d0aa650bde11a9
john48k⌖ <126651207+john48k@users.noreply.github.com>
2024-12-08 12:11:24 +0800

    Style New Prompt Draft 1

--- src/components/new-prompt/new-prompt.jsx
+++ src/components/new-prompt/new-prompt.jsx
@@ -3,7 +3,16 @@ import "./new-prompt.css";
 const NewPrompt = () => {
   return (
     <div className="newPrompt">
-      <p>Enter your prompt here</p>
+      <form action="newForm">
+        <label htmlFor="file">
+          <img src="/attachment.png" alt="" />
+        </label>
+        <input id="file" type="file" multiple={false} hidden />
+        <input type="text" placeholder="Ask me anything..." />
+        <button>
+          <img src="/arrow.png" alt="" />
+        </button>
+      </form>
     </div>
   );
 };
